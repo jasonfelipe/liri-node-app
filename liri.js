1c65e27450d154a58d4a6bc0b7854e8c3b3e7c64
Legend <jasonrfelipe@gmail.com>
2018-07-11 13:56:48 -0700

    cleaned up console logs, added fail states to Spotify and movie search

--- liri.js
+++ liri.js
@@ -35,44 +35,91 @@ if (command === 'spotify-this-song') {
 
     spotify.search({ type: 'track', query: songName }, function (err, data) {
         if (err) {
-            console.log('Sorry, an error occurred: ' + err);
+            console.log(
+                "______________________________________________________\n" +
+                'Sorry, an error occurred: \n' + err +
+                "\nHow about you listen Ace of Base instead? \nI recommend The Sign! :)"
+            );
+            spotify.search({ type: 'album', query: "Happy Nation" }, function (err2, data2) {
+                if (!err2) {
+                    console.log(
+                        "\nArtist: " + data2.albums.items[0].artists[0].name,
+                        "\nAlbum: " + data2.albums.items[0].name,
+                        "\nSpotify URL: " + data2.albums.items[0].external_urls.spotify
+
+
+                    )
+                }
+            });
+
             return;
         }
-
         console.log(
-        "______________________________________________________\n"+
-        "Artist's Name: " + data.tracks.items[0].artists[0].name,
-        "\nAlbum Name: " + data.tracks.items[0].album.name,
-        "\nSpotify Link: " + data.tracks.items[0].external_urls.spotify
+            "______________________________________________________\n" +
+            "Song Name: " + data.tracks.items[0].name,
+            "\nArtist: " + data.tracks.items[0].artists[0].name,
+            "\nAlbum Name: " + data.tracks.items[0].album.name,
+            "\nSpotify Link: " + data.tracks.items[0].external_urls.spotify
         );
     });
 
-}   
+}
 
 //node liri.js movie-this
 if (command === 'movie-this') {
     var movie = process.argv[3];
 
-    request("https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-            if (JSON.parse(body).Response != "False") {
-                console.log("____________________________\n");
-                console.log("Here is your movie. :) \n")
-                console.log("The movie title is: " + JSON.parse(body).Title);
-                console.log("Movie release date is: " + JSON.parse(body).Year);
-                console.log("This movie was created in: " + JSON.parse(body).Country);
-                console.log("The Rotten Tomatoes Rating is: " + JSON.parse(body).Ratings[1].Value);
-                console.log("The movie's IMDB rating is: " + JSON.parse(body).imdbRating);
-                console.log("\nHere are the actors: \n" + JSON.parse(body).Actors + "\n");
-                console.log("Here is the plot: \n" + JSON.parse(body).Plot);
+    //If no movie is searched.
+    if (process.argv[3] === undefined) {
+        request("https://www.omdbapi.com/?t=Mr%20Nobody&y=&plot=short&apikey=trilogy", function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                console.log(
+                    "____________________________\n" +
+                    "I see you didn't put a movie in. \n"+
+                    "Well I recommend Mr. Nobody! It's on Netflix!\n",
+                    "\nThe movie title is: " + JSON.parse(body).Title,
+                    "\nMovie release date is: " + JSON.parse(body).Year,
+                    "\nThis movie was created in: " + JSON.parse(body).Country,
+                    "\nThe Rotten Tomatoes Rating is: " + JSON.parse(body).Ratings[1].Value,
+                    "\nThe movie's IMDB rating is: " + JSON.parse(body).imdbRating + "\n",
+                    "\nHere are the actors: \n" + JSON.parse(body).Actors + "\n" +
+                    "\nHere is the plot: \n" + JSON.parse(body).Plot
+
+                );
+
             }
 
-            else {
-                console.log("____________________________");                
-                console.log("Sorry I cannot find your movie! :(");
+        });
+    }
+    else {
+        //takes the user input. 
+        request("https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+
+                //if the response isn't nothing.
+                if (JSON.parse(body).Response != "False") {
+                    console.log(
+                        "____________________________\n" +
+                        "Here is your movie. :)\n",
+                        "\nThe movie title is: " + JSON.parse(body).Title,
+                        "\nMovie release date is: " + JSON.parse(body).Year,
+                        "\nThis movie was created in: " + JSON.parse(body).Country,
+                        "\nThe Rotten Tomatoes Rating is: " + JSON.parse(body).Ratings[1].Value,
+                        "\nThe movie's IMDB rating is: " + JSON.parse(body).imdbRating + "\n",
+                        "\nHere are the actors: \n" + JSON.parse(body).Actors + "\n" +
+                        "\nHere is the plot: \n" + JSON.parse(body).Plot
+                    );
+
+                }
+                //if it comes up with nothing.
+                else {
+                    console.log("____________________________");
+                    console.log("Sorry I cannot find your movie! :(");
+                }
             }
-        }
-    });
+
+        });
+    }
 }
 
 //do-what-it-says
