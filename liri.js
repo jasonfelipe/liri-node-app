a3c2d6544f4b8b509157cc6860798a60bb619a92
Legend <jasonrfelipe@gmail.com>
2018-07-10 18:46:48 -0700

    got spotify to work, cleaned up some code

--- liri.js
+++ liri.js
@@ -30,19 +30,19 @@ if (command === 'my-tweets') {
 
 //node liri.js spotify-this-song
 if (command === 'spotify-this-song') {
-    const spotify = new spotify(keys.spotify);
+    const spotify = new Spotify(keys.spotify);
     const songName = process.argv[3];
 
     spotify.search({ type: 'track', query: songName }, function (err, data) {
         if (err) {
-            console.log('Error occurred: ' + err);
+            console.log('Sorry, an error occurred: ' + err);
             return;
         }
 
-        console.log(data);
+        console.log(data.tracks.items[0]);
     });
 
-}
+}   
 
 //node liri.js movie-this
 if (command === 'movie-this') {
@@ -51,14 +51,14 @@ if (command === 'movie-this') {
     request("https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
         if (!error && response.statusCode === 200) {
             if (JSON.parse(body).Response != "False") {
-                console.log("____________________________");
+                console.log("____________________________\n");
                 console.log("Here is your movie. :) \n")
                 console.log("The movie title is: " + JSON.parse(body).Title);
                 console.log("Movie release date is: " + JSON.parse(body).Year);
                 console.log("This movie was created in: " + JSON.parse(body).Country);
                 console.log("The Rotten Tomatoes Rating is: " + JSON.parse(body).Ratings[1].Value);
                 console.log("The movie's IMDB rating is: " + JSON.parse(body).imdbRating);
-                console.log("\nHere are the actors: " + JSON.parse(body).Actors + "\n");
+                console.log("\nHere are the actors: \n" + JSON.parse(body).Actors + "\n");
                 console.log("Here is the plot: \n" + JSON.parse(body).Plot);
             }
 
