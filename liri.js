7163bd931d9c41385ae597bdf9f48df3fd9c829c
Legend <jasonrfelipe@gmail.com>
2018-07-12 19:12:32 -0700

    added do-what-it-says code, not functionable yet, but soon

--- liri.js
+++ liri.js
@@ -4,78 +4,48 @@ const keys = require('./keys.js');
 const Twitter = require('twitter');
 const request = require('request');
 const Spotify = require('node-spotify-api');
+const fs = require('fs');
+
 
 //variable for the command.
-const command = process.argv[2];
+let command = process.argv[2];
 
 
-
-//-----The Commands-----
-
-//node liri.js my-tweets
-if (command === 'my-tweets') {
-    const client = new Twitter(keys.twitter);
-    console.log("____________________________");
-    console.log("Here are your latest tweets. :)");
-    const params = { screen_name: 'heyjasonf' };
-    client.get('statuses/user_timeline', params, function (error, tweets, response) {
-        if (!error) {
-            for (i = 0; i < tweets.length; i++) {
-                console.log("\n" + tweets[i].created_at + "\n   " + tweets[i].text);
-            }
-        }
-    });
-
-}
-
-//node liri.js spotify-this-song
-if (command === 'spotify-this-song') {
-    const spotify = new Spotify(keys.spotify);
-    const songName = process.argv[3];
-
-    spotify.search({ type: 'track', query: songName }, function (err, data) {
+//-----Functions-----
+function randomCommand() {
+    fs.readFile("random.txt", "utf8", function (err, data) {
         if (err) {
-            console.log(
-                "______________________________________________________\n" +
-                'Sorry, an error occurred: \n' + err +
-                "\nHow about you listen Ace of Base instead? \nI recommend The Sign! :)"
-            );
-            spotify.search({ type: 'album', query: "Happy Nation" }, function (err2, data2) {
-                if (!err2) {
-                    console.log(
-                        "\nArtist: " + data2.albums.items[0].artists[0].name,
-                        "\nAlbum: " + data2.albums.items[0].name,
-                        "\nSpotify URL: " + data2.albums.items[0].external_urls.spotify
-
-
-                    )
-                }
-            });
-
-            return;
+            return console.log(err);
         }
-        console.log(
-            "______________________________________________________\n" +
-            "Song Name: " + data.tracks.items[0].name,
-            "\nArtist: " + data.tracks.items[0].artists[0].name,
-            "\nAlbum Name: " + data.tracks.items[0].album.name,
-            "\nSpotify Link: " + data.tracks.items[0].external_urls.spotify
-        );
-    });
 
+        var dataArr = data.split(",");
+
+        randomCommand = dataArr[0];
+        searchItem = dataArr[1];
+
+        if (randomCommand === 'spotify-this-song') {
+            dataArr.push(process.argv);
+            console.log(process.argv)            
+            spotifyThis();
+        }
+        if (randomCommand === 'movie-this') {
+            movieThis(searchItem);
+        }
+
+    });
 }
 
-//node liri.js movie-this
-if (command === 'movie-this') {
-    var movie = process.argv[3];
 
+function movieThis() {
+
+    let movie = process.argv[3];
     //If no movie is searched.
     if (process.argv[3] === undefined) {
         request("https://www.omdbapi.com/?t=Mr%20Nobody&y=&plot=short&apikey=trilogy", function (error, response, body) {
             if (!error && response.statusCode === 200) {
                 console.log(
                     "____________________________\n" +
-                    "I see you didn't put a movie in. \n"+
+                    "I see you didn't put a movie in. \n" +
                     "Well I recommend Mr. Nobody! It's on Netflix!\n",
                     "\nThe movie title is: " + JSON.parse(body).Title,
                     "\nMovie release date is: " + JSON.parse(body).Year,
@@ -122,9 +92,86 @@ if (command === 'movie-this') {
     }
 }
 
+function spotifyThis() {
+    const spotify = new Spotify(keys.spotify);
+    let songName = process.argv[3];
+    if (songName === undefined) {
+        console.log(
+            "Please input a song name!\nMake sure it's in quotes too. Ex: 'song name'"
+        )
+    }
+    else {
+        spotify.search({ type: 'track', query: songName }, function (err, data) {
+            if (err) {
+                console.log(
+                    "______________________________________________________\n" +
+                    'Sorry, an error occurred: \n' + err +
+                    "\nHow about you listen Ace of Base instead? \nI recommend The Sign! :)"
+                );
+                spotify.search({ type: 'album', query: "Happy Nation" }, function (err2, data2) {
+                    if (!err2) {
+                        console.log(
+                            "\nArtist: " + data2.albums.items[0].artists[0].name,
+                            "\nAlbum: " + data2.albums.items[0].name,
+                            "\nSpotify URL: " + data2.albums.items[0].external_urls.spotify
+
+
+                        )
+                    }
+                });
+
+                return;
+            }
+            console.log(
+                "______________________________________________________\n" +
+                "Song Name: " + data.tracks.items[0].name,
+                "\nArtist: " + data.tracks.items[0].artists[0].name,
+                "\nAlbum Name: " + data.tracks.items[0].album.name,
+                "\nSpotify Link: " + data.tracks.items[0].external_urls.spotify
+            );
+        });
+    }
+};
+
+function displayTweets() {
+    let client = new Twitter(keys.twitter);
+    console.log("____________________________");
+    console.log("Here are your latest tweets. :)");
+    const params = { screen_name: 'heyjasonf' };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            for (i = 0; i < tweets.length; i++) {
+                console.log("\n" + tweets[i].created_at + "\n   " + tweets[i].text);
+            }
+        }
+    });
+
+}
+
+//------------------------------------------------
+
+//-----The Commands-----
+
+//node liri.js my-tweets
+if (command === 'my-tweets') {
+    displayTweets();
+}
+
+//node liri.js spotify-this-song
+if (command === 'spotify-this-song') {
+    spotifyThis();
+
+}
+
+//node liri.js movie-this
+if (command === 'movie-this') {
+    movieThis();
+}
+
 //do-what-it-says
+
 if (command === 'do-what-it-says') {
-    console.log('Hey this is a command to do whatever');
+    randomCommand();
 }
 //-------------------------
 
